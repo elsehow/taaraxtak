4261ffeac068e562f75f4e436a7caf80d01b863c
elsehow <yes@cosmopol.is>
2021-05-27 15:02:25 -0700

    few more tests

--- src/ooni/utils.py
+++ src/ooni/utils.py
@@ -1,9 +1,79 @@
+import logging
+import coloredlogs
+import requests
+from time import sleep
 from datetime import datetime
 import pytz
+import pandas as pd
+import socket
+import urllib.parse
+
+from typing import Optional
+from psycopg2.extensions import cursor
+from psycopg2.extensions import connection
 
 def to_utc (t: datetime) -> datetime:
     return t.astimezone(pytz.utc)
 
-# TODO make this dry!
+# TODO make this dry! it's shared with w3techs rn
 def is_nonempty_str(my_str: str) -> bool:
-    return (type(my_str) == str) & (len(my_str) > 0)
+    is_str = type(my_str) == str
+    if is_str:
+        return len(my_str) > 0
+    return False
+
+#
+# OONI querying
+#
+
+def api_query (query: str, results=[], queries=1, max_queries=None) -> list:
+    '''Recursively query the API, up to `max_queries`. (If `max_queries=None`, we
+    will paginate through the results as long as they run).
+    '''
+    base_url = 'https://api.ooni.io/api/v1/'
+    query = '{!s}{!s}'.format(base_url, query)
+    try:
+        resp =  requests.get(query).json()
+        results = results + resp['results']
+        next_url = resp['metadata']['next_url']
+        if max_queries is not None and queries > max_queries:
+            return results
+        if next_url:
+            # sleep so as to not overwhelm the endpoint
+            sleep(config['sleep-times']['ooni-paginate'])
+            # remove base url to perfrom the query
+            next_url = next_url.split('api/v1/')[1]
+            return api_query(next_url, results, queries+1, max_queries)
+        return results
+    except Exception as inst:
+        # if we have an error,
+        logging.warning("Error querying API: {!s}".format(inst))
+        # just return what we've collected
+        # (at worst, `results` will be `[]`)
+        return results
+
+def query_recent_measurements (max_queries=5) -> list:
+    '''Queries all recent measurements, up to specified maximum number of queries.'''
+    base_query = 'measurements?test_name=web_connectivity&anomaly=true&order_by=test_start_time&limit=1000'
+    return api_query(base_query, max_queries=max_queries)
+
+def get_blocking_type (measurement) -> str:
+    '''Get blocking type, if available.'''
+    try:
+        return measurement['scores']['analysis']['blocking_type']
+    except:
+        return None
+
+#
+# Get IP from URL
+#
+
+def get_hostname (url):
+    return urllib.parse.urlparse(url).netloc
+
+def fetch_ip_from_hostname (hostname: str) ->  str:
+    try:
+        return socket.gethostbyname(hostname)
+    except Exception as inst:
+            logger.warning(f"Error looking up IP of hostname {hostname}: {inst}")
+            return None
--- test/ooni-test.py
+++ test/ooni-test.py
@@ -1,6 +1,7 @@
 import psycopg2
 import testing.postgresql
 import pytest
+from IPy import IP
 
 from datetime import datetime
 
@@ -31,12 +32,37 @@ def postgresdb (request):
 def test_to_utc ():
     t = datetime(2021, 5, 27, 21, 40, 17, 486566)
     t_utc = ooni_utils.to_utc(t)
-    return t_utc.hour == 4
+    assert(t_utc.hour == 4)
 
-def alpha2_codes ():
+def test_is_nonempty_str ():
+    ok = ooni_utils.is_nonempty_str('hi')
+    assert(ok == True)
+    not_ok = ooni_utils.is_nonempty_str('')
+    assert(not_ok == False)
+    not_ok = ooni_utils.is_nonempty_str(5)
+    assert(not_ok == False)
+
+def test_alpha2_codes ():
     ok = ooni_types.Alpha2('US')
+    assert(str(ok) == 'US')
     with pytest.raises(Exception):
         not_ok = ooni_types.Alpha2('blah')
         not_ok = ooni_types.Alpha2()
-        not_ok = ooni_types.Alpha2('5')
+        not_ok = ooni_types.Alpha2(5)
+
+# def test_query_ooni ():
+#     ms = ooni_utils.query_recent_measurements(max_queries=1)
+#     assert(len(ms) > 1)
+#     # should not error
+#     ooni_utils.get_blocking_type(ms[0])
+
+def test_get_hostname ():
+    assert(ooni_utils.get_hostname('http://www.wikipedia.org')=='wikipedia.org')
+
+def test_fetch_ip ():
+    hn = ooni_utils.get_hostname('http://www.wikipedia.org')
+    maybe_ip = ooni_utils.fetch_ip_from_hostname(hn)
+    # should not error
+    IP(maybe_ip)
+
 
